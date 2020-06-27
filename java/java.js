5da6d5cd5b314386dcf11633763028a34506034b
ashabjannat <2alifashabuljannat@gmail.com>
2020-06-27 23:41:31 +0600

    first commit2

---
+++ java/java.js
@@ -0,0 +1,50 @@
+//  document.write("alif-js");
+//  var a = " alif ";
+//  document.write(a);    
+//  document.write("<br>alif java");
+//  document.write(a);
+//  document.write("<br>");
+
+
+/*var a = 2;
+var b = 3 
+var c = 4;
+a++;
+var a =(a+b)*c/10;
+var alif =a;*/
+
+
+// var a = 10;
+// var b = 10;
+// a++;a+=5;
+// if(a > b){
+//  document.write(a);
+// }
+
+
+// var a = 40;
+// var b = 15;b=b+5;
+// if(a>=30 && b<=20){
+//      document.write(a,b);
+//  }
+//  document.write("<br>");
+//  a=a-12;
+// if(a>=30 || b<=20){
+//     document.write(a,b);
+// }
+
+
+// $('p').click(function() {
+//     alert('my roles ');
+// });
+$('button').click(function() {
+    $('p').toggle();
+});
+
+
+// $('p').click(function() {
+//     alert('my roles ');
+// });
+// $('button').click(function() {
+//     $('p').toggle();
+// });
